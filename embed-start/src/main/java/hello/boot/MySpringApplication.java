f3ea398016a0b24fa9e55fceef3d6252f105ec60
forteleaf <forteleaf@gmail.com>
2023-03-28 08:09:00 +0900

    make application like spring boot

---
+++ embed-start/src/main/java/hello/boot/MySpringApplication.java
@@ -0,0 +1,38 @@
+package hello.boot;
+
+import hello.spring.HelloConfig;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class MySpringApplication {
+    public static void run(Class configClass, String[] args) {
+        System.out.println("MySpringApplication.run args" + args);
+
+        // tomcat 설정
+        Tomcat    tomcat    = new Tomcat();
+        Connector connector = new Connector();
+        connector.setPort(8080);
+        tomcat.setConnector(connector);
+
+        // spring container 설정
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(HelloConfig.class);
+
+        // spring MBV dispather servlet 생성, spring cantainer 등록
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(appContext);
+
+        Context context = tomcat.addContext("", "/");
+        tomcat.addServlet("", "dispatcher", dispatcherServlet);
+        context.addServletMappingDecoded("/hello-spring", "dispatcher");
+
+        try {
+            tomcat.start();
+        } catch (LifecycleException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
