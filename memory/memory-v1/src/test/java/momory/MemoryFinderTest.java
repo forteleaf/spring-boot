0ff0ee7ae954e94219633013f4efe52b1347ad6b
forteleaf <forteleaf@gmail.com>
2023-03-29 23:20:03 +0900

    Start library project

---
+++ memory/memory-v1/src/test/java/momory/MemoryFinderTest.java
@@ -0,0 +1,16 @@
+package momory;
+
+import memory.Memory;
+import memory.MemoryFinder;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemoryFinderTest {
+
+	@Test
+	void get() {
+		MemoryFinder memoryFinder = new MemoryFinder();
+		Memory       memory       = memoryFinder.get();
+		Assertions.assertThat(memory).isNotNull();
+	}
+}
